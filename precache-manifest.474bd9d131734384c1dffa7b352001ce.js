c699e26371649ca31d44eb36d6b1befce5dbdace
jirayus15601 <jirayus_kard@cmu.ac.th>
2020-03-24 22:14:41 +0700

    Updates

---
+++ precache-manifest.474bd9d131734384c1dffa7b352001ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0498028e650e5c8da2e405dfb7e87f8c",
+    "url": "/jirayus-kard/hw8-2/index.html"
+  },
+  {
+    "revision": "d089ae50e10978caad40",
+    "url": "/jirayus-kard/hw8-2/static/css/main.4851e9fc.chunk.css"
+  },
+  {
+    "revision": "e817c732f3fb153ee1e5",
+    "url": "/jirayus-kard/hw8-2/static/js/2.a135969c.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/jirayus-kard/hw8-2/static/js/2.a135969c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d089ae50e10978caad40",
+    "url": "/jirayus-kard/hw8-2/static/js/main.c1eb84ff.chunk.js"
+  },
+  {
+    "revision": "ff0744c9c9b072fc4199",
+    "url": "/jirayus-kard/hw8-2/static/js/runtime-main.a6e52c17.js"
+  }
+]);
